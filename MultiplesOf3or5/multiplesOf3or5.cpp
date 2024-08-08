0c30935e40c905d7d0bf6873860453dc8572e5c1
ltlexi140 <ltlexi1408@proton.me>
2024-08-08 21:49:42 +1000

    finished problem

--- MultiplesOf3or5/multiplesOf3or5.cpp
+++ MultiplesOf3or5/multiplesOf3or5.cpp
@@ -6,15 +6,17 @@ int main(){
     int result = 0;
     std::vector<int> list;
 
-    for(int i = 1; i <= 10; i++){
+    for(int i = 1; i < 10; i++){
         if(i % 3 == 0 || i % 5 == 0){
             list.push_back(i);
         }
     }
 
     for(const int& i : list){
-        std::cout << i << std::endl;
+        result += i;
     }
+
+    std::cout << result << std::endl;
 
     return 0;
 }
